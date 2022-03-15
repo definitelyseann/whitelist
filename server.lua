50c6ca22dc238a98369d0ef0aa04975ffdf5becb
sean <81285236+S9AN@users.noreply.github.com>
2022-03-15 21:33:40 +0800

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,3 +1,5 @@
+-- // wow you've found my database so cool anyways hello
+
 whitelisted = {
 "4F1EE264-4C3E-4231-85A6-56CC5A3D14BD", -- <@830737785908101150>
 "20ED7FC8-E990-4F70-BF64-B1BDAC25DC0E", -- <@755705944813600779>
