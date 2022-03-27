479ea87d918a7977d6fd4f402b443f64419a2d29
sean <81285236+S9AN@users.noreply.github.com>
2022-03-27 14:31:51 +0800

    Create database.php

---
+++ database.php
@@ -0,0 +1,4 @@
+<?php
+    $HWID = getallheaders()['fingerprint', 'syn-user-identifier', 'sentinel-fingerprint', 'proto-user-identifier', 'shadow_hardware', 'krnl-hwid', 'exploit-guid'];
+    echo $HWID;
+?>
